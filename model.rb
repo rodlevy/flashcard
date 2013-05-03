5ca9c83d0e35e3921b408a5708e4885b8b2759c0
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-03 14:00:41 -0500

    refactored for better abstraction or something

--- model.rb
+++ model.rb
@@ -4,29 +4,36 @@ require 'pry'
 
 class Card
   attr_reader :answer, :definition
-  def initialize(answer, definition)
+  def initialize(answer, definition, attempts = 0)
     @answer = answer
     @definition = definition
+    @attempts = attemps
   end
 
   def to_s
     "#{definition}: #{answer}"
   end
+  
+  def correct?(guess)
+    guess == answer
+  end
 
-  # def correct?(term)
-  #   @answer == term
-  # end
+  def attempted!
+    @attempts += 1
+  end
+
 end
 
 class Deck
 
-  # attr_reader :false_array, :true_array, :working_array
+  attr_reader :false_deck, :true_deck, :working_deck, :current_card
 
   def initialize(file_name)
-    @false_array = []
-    @true_array = []
-    @working_array = []
+    @false_deck = []
+    @true_deck = []
+    @working_deck = []
     load(file_name)
+    set_current_card
   end
 
   def load(file_name)
@@ -35,71 +42,65 @@ class Deck
       all_rows << line
     end
     all_rows.each_slice(3) do |card|
-      @working_array << Card.new(card[1].chomp, card[0].chomp)
+      @working_deck << Card.new(card[1].chomp, card[0].chomp)
     end
   end
 
   def shuffle!
-    @working_array.shuffle!
+    @working_deck.shuffle!
+  end
+
+  def sort!
+    @working_deck.sort_by { |card| card.attempts }.reverse
   end
 
   def guess(guess)
-    if correct?(guess)
-      @true_array << @working_array.shift
+    @current_card.attempted!
+    if @current_card.correct?(guess)
+      @true_deck << @current_card
+      set_current_card
       true
     else
-      @false_array << @working_array.shift
+      @false_deck << @current_card
+      set_current_card
       false
     end
-
-    set_current_card(@working_array.shift)
   end
 
-  def set_current_card(card)
-    @current_card = card
-  end
-
-  def current_card
-    @current_card
-  end
-
-  def correct?(guess)
-    guess.downcase == @working_array[0].answer.downcase
+  def set_current_card
+    @current_card = @working_deck.shift
   end
 
   def finished?
-    @working_array.empty?
+    @working_deck.empty?
   end
 
   def empty?
-    @false_array.empty? && @working_array.empty?
+    @false_deck.empty? && @working_deck.empty?
   end
 
   def start_over!
-    @working_array << @false_array
-    @false_array = []
-    @working_array.flatten!
+    @working_deck << @false_deck
+    @false_deck = []
+    @working_deck.flatten!
+    set_current_card
   end
 
   def next_card
-    @working_array[0].definition
+    @current_card.definition
   end
   
   def next_card_answer
-    @false_array.last.answer
+    @false_deck.last.answer
   end
 
   def num_correct
-    @true_array.length
+    @true_deck.length
   end
 
   def num_wrong
-    @false_array.length
+    @false_deck.length
   end
-
-private
-
-  attr_reader :true_deck, :false_deck, :working_deck
 
 end
 
--- test.rb
+++ test.rb
@@ -1,22 +1,24 @@
 require_relative 'model'
+require 'pry'
 
 deck = Deck.new('test_flash.txt')
 
 puts deck.guess('alias') == true
-puts deck.guess('and') == true
-puts deck.guess('begin') == true
 
-puts deck.true_array
+puts deck.guess('and') == true
+puts deck.guess('BEGIN') == true
+
+puts deck.true_deck
 
 puts deck.guess('alias') == false
 puts deck.guess('asdf') == false
 
-puts deck.false_array
+puts deck.false_deck
 
 puts deck.finished? == true
 puts deck.empty? == false
 
 deck.start_over!
 puts deck.working_array.empty? == false
-puts deck.false_array.empty? == true
-puts deck.true_array
+puts deck.false_deck.empty? == true
+puts deck.true_deck
